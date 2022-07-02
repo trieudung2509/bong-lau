7a4cd3c6f1eca0ba9ebe927aaa2dc8f9b8d26c3f
Lucas Mario <lucas.brightsoft@gmail.com>
2022-07-02 13:11:08 +0700

    feat: add 4 block in home page

--- index.php
+++ index.php
@@ -22,8 +22,145 @@
 						</div>
 				</div>
 			</div>
+	</div>
 
+	<div class="feature-block">
+		<div class="container">
+			<div class="row">
+					<div class="col-12 feature-title">
+							<h2>Những tính năng nổi bật</h2>
+					</div>
+			</div>
+			<div class="row list-feature">
+					<div class="col-md-4">
+							<div class="feature-item">
+								<img src="images/feature-1.png" alt="">
+								<h6>Xem trực tiếp</h6>
+								<p>
+									Tận hưởng những diễn biến trực tiếp của từng trận đấu thuộc các giải đấu hàng đầu thế giới.
+								</p>
+							</div>
+					</div>
+					<div class="col-md-4">
+							<div class="feature-item">
+								<img src="images/feature-2.png" alt="">
+								<h6>Cập nhật tỷ số</h6>
+								<p>
+								Tỷ số trận đấu được cập nhật tức thì, bạn hoàn toàn có thể xem tỷ số của những trận đã và đang diễn ra một cách nhanh nhất.
+								</p>
+							</div>
+					</div>
+					<div class="col-md-4">
+							<div class="feature-item">
+								<img src="images/feature-3.png" alt="">
+								<h6>Xem lịch thi đấu</h6>
+								<p>
+								Lịch thi đấu được Thẻ vàng cập nhật trước [..] ngày để bạn có thể sắp xếp thời gian đón xem trận đấu yêu thích
+								</p>
+							</div>
+					</div>
+					<div class="col-md-4">
+							<div class="feature-item">
+								<img src="images/feature-4.png" alt="">
+								<h6>Xem highlight</h6>
+								<p>
+								Nếu bạn bỏ lỡ một trận đấu hay, đừng lo lắng! Mục highlight sẽ giúp bạn nắm bắt những diễn biến chính của trận đó.
+								</p>
+							</div>
+					</div>
+					<div class="col-md-4">
+							<div class="feature-item">
+								<img src="images/feature-5.png" alt="">
+								<h6>Cập nhật bảng xếp hạng</h6>
+								<p>
+								Xem bảng xếp hạng theo từng giải đấu và xem thứ hạng của đội bóng bạn yêu thích.
+								</p>
+							</div>
+					</div>
+			</div>
+		</div>
+	</div>
 
+	<div class="social-block">
+		<div class="container">
+			<div class="row">
+					<div class="col-lg-7 col-12">
+						<div class="social-text">
+							<h2>Tải ứng dụng để tận hưởng những tính năng hấp dẫn này nhé!</h2>
+							<p>
+								<a href="">
+									<img src="images/social-download.png" alt="">
+								</a>
+								<a href="">
+									<img src="images/social-gg.png" alt="">
+								</a>
+							</p>
+						</div>
+					</div>
+					<div class="col-lg-5 col-12">
+							<div class="social-img">
+								<img src="images/bg-messi.png" alt="">
+							</div>
+					</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="tournament-block">
+			<div class="container">
+				<div class="row">
+						<div class="col-12">
+								<div class="tournament-title">
+										<h2>Thẻ vàng chiếu phát những giải đấu nào?</h2>
+								</div>
+						</div>
+				</div>
+				<div class="row tournament-list">
+					<div class="col-lg-3 col-md-6 col-6 tournament-item">
+							<div class="tournament-image">
+								<img src="images/champions.png" alt="">
+							</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-6 tournament-item">
+							<div class="tournament-image">
+								<img src="images/europa.png" alt="">
+							</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-6 tournament-item">
+							<div class="tournament-image">
+								<img src="images/premier-league.png" alt="">
+							</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-6 tournament-item">
+							<div class="tournament-image">
+								<img src="images/laliga.png" alt="">
+							</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-6 tournament-item">
+							<div class="tournament-image">
+								<img src="images/seria.png" alt="">
+							</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-6 tournament-item">
+							<div class="tournament-image">
+								<img src="images/bundesliga.png" alt="">
+							</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-6 tournament-item">
+							<div class="tournament-image">
+								<img src="images/ligue-1.png" alt="">
+							</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-6 tournament-item">
+							<div class="tournament-image">
+								<img src="images/word-cup.png" alt="">
+							</div>
+					</div>
+				</div>
+				<div class="tournament-footer">
+						<p>Và hơn 200 giải đấu quốc nội và quốc tế trên phạm vi toàn cầu.</p>
+				</div>
+			</div>
 	</div>
 
 	<?php include "templates/footer.php"; ?>
