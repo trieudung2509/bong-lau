6a2993b9c7a95a9927df08e43af3e14ac642a1f8
DangKhoa96 <khoanguyenptit96@gmail.com>
2022-07-03 21:35:20 +0700

    merge master

--- templates/main-2.php
+++ templates/main-2.php
@@ -1,3 +1,73 @@
-<div class="test">
-  <p>nguyen dang khoa hoc vien cong nghe buu chinh vien thong</p>
+<div class="why-choose-me">
+  <div class="container block-1">
+    <h2 class="block-title">Bạn không thể bỏ qua Bông Lau vì…</h2>
+    <div class="block-content">
+      <div class="left-content">
+        <div class="content-item">
+          <img src="./images/icon-checked.png" class="icon-checked" />
+          <div class="content-detail">
+            <h3 class="title">Hoàn toàn miễn phí</h3>
+            <p class="description">Bạn không phải trả bất kỳ khoản phí nào để tải và sử dụng ứng dụng này.</p>
+          </div>
+        </div>
+        <div class="content-item">
+          <img src="./images/icon-checked.png" class="icon-checked" />
+          <div class="content-detail">
+            <h3 class="title">Hoàn toàn miễn phí</h3>
+            <p class="description">Stream chất lượng cao mang đến hình ảnh sắc nét, âm thanh sống động và đặc biệt cực
+              kỳ ít tình trạng giật lag.</p>
+          </div>
+        </div>
+        <div class="content-item">
+          <img src="./images/icon-checked.png" class="icon-checked" />
+          <div class="content-detail">
+            <h3 class="title">Cực kỳ tiện lợi</h3>
+            <p class="description">Ứng dụng này giúp bạn có thể xem bóng đá trên nhiều thiết bị khác nhau, tại bất cứ
+              đâu và bất cứ khi nào.</p>
+          </div>
+        </div>
+        <div class="content-item">
+          <img src="./images/icon-checked.png" class="icon-checked" />
+          <div class="content-detail">
+            <h3 class="title">Cực kỳ dễ sử dụng</h3>
+            <p class="description">Ứng dụng này được thiết kế để bạn có thể thao tác một cách nhanh chóng và dễ dàng
+              nhất. Xem trận đấu, tỷ số, lịch thi đấu, highlight… tất cả chỉ trong vài thao tác.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="right-content">
+      </div>
+    </div>
+  </div>
+
+  <div class="container block-2">
+    <div class="content-image">
+      <img src="./images/person.png" class="person" alt="" />
+      <div class="content-wrapper">
+        <div class="content">
+          <img src="./images/icon-light.png" class="icon-light" alt="" />
+          <h3 class="title">Bạn có biết?</h3>
+          <p class="description">Xem bóng đá online trên app giúp đảm bảo độ ổn định đường truyền và chất lượng cao hơn
+            trên web tớ<span> 1.5 lần!</span></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container block-3">
+    <div class="right-content">
+      <div class="content-wrapper">
+        <h3 class="title">Tải Bông Lau ngay thôi!!!!!</h3>
+        <div class="list-download">
+          <a href="#" class="download-item">
+            <img src="./images/image-download-androi.png" alt="" />
+          </a>
+          <a href="#" class="download-item">
+            <img src="./images/image-download-ios.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
